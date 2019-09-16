aca67ee8a2c0a6a85f08441880e916eaa76a3be7
Sam Belliveau <sam.belliveau@gmail.com>
2019-09-16 17:37:05 -0400

    Add first idea for Lesson #1

---
+++ 2020/Lesson #1/Lesson.java
@@ -0,0 +1,26 @@
+
+class Lesson {
+
+    public static void main(String args[]) {
+
+        // TODO: Explain Comments
+
+
+        // TODO: Create a double and string
+
+
+        // TODO: Print out the variables
+
+
+        // TODO: Introduce Conditions
+        // TODO: Use If with Conditionals
+
+
+        // TODO: Make a While Loop and count 1 - 10
+
+        
+        // TODO: Make a function if you have time
+
+    }
+
+}
