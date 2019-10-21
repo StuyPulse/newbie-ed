35bee58304c920ccf7831b76984212a6b5b90ad1
Renee <rmui10@stuy.edu>
2019-10-20 21:42:13 -0400

    Add code from first motor lesson and vendordeps

--- 2020/Lesson #2/src/main/java/frc/robot/Robot.java
+++ 2020/Lesson #2/src/main/java/frc/robot/Robot.java
@@ -7,14 +7,26 @@
 
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
+
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
 
+  private WPI_VictorSPX shooterMotor;
+
   @Override
   public void robotInit() {
+    //makes new motor at port 13
+    shooterMotor = new WPI_VictorSPX(13);
+
+    //inverts the motor
+    shooterMotor.setInverted(true);
+
+    //makes motor coast to a stop when given speed of 0
+    //can also be set to NeutralMode.Brake to make motor stop when given speed of 0
+    shooterMotor.setNeutralMode(NeutralMode.Coast);
   }
 
   @Override
@@ -31,6 +43,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void teleopPeriodic() {
+    //sets motor speed to 0.25
+    //speed ranges from -1 to 1 (full speed backwards to full speed forwards)
+    shooterMotor.set(0.25);
   }
 
   @Override
