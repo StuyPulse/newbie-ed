5d546c3de01f70bee4c0ed2fa367a9a1aa0874cb
KevinLi0711 <kli30@stuy.edu>
2020-11-10 17:40:57 -0500

    add Main.java

---
+++ 2021/03_java/kevin_li/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+double y = 2.2;
+boolean z = true;
+System.out.println(y);
+System.out.println(z);
+for (int x = 0; x < 10; x++) {System.out.println(x);}
+for (int a = 0; a < 10; a+=2) {System.out.println(a);}
+    }
+}
