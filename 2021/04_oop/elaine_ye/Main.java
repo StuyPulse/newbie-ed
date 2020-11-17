4855292fa171905368f65f58cbce9d1c99222473
elaine-ye <elaineye2006@gmail.com>
2020-11-17 17:41:13 -0500

    Add Main.java and Car.java

---
+++ 2021/04_oop/elaine_ye/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+  public static void main(String[] args) {
+    Car shinyCar = new Car("Toyota");
+    System.out.println(shinyCar);
+
+    for (int i = 0; i < 21; i++) {
+      shinyCar.drive();
+    }
+
+      shinyCar.repair();
+      for (int i = 0; i < 21; i++) {
+        shinyCar.drive();
+      }
+  }
+}
