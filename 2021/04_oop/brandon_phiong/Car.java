bf7d61065e3f6d5c0edbaebad6f87b53a1e44517
Brandon Phiongl <onename548@gmail.com>
2020-11-17 20:47:03 -0500

    Made Car.java

---
+++ 2021/04_oop/brandon_phiong/Car.java
@@ -0,0 +1,45 @@
+public class Car {
+  private String model;
+  private String color;
+  private int mileage = 0;
+  private static final int MAX_MILEAGE = 20;
+
+  public Car(String model, String color) {
+    this.model = model;
+    this.color = color;
+  }
+
+  public Car(String model) {
+    this.model = model;
+    this.color = "colorless";
+  }
+
+  public boolean willExplode() {
+    return mileage >= MAX_MILEAGE;
+
+  }
+
+  public void repair() {
+    mileage = 0;
+    System.out.println("Repaired!");
+  }
+
+  private void explode() {
+    System.out.println("5");
+    System.out.println("4");
+    System.out.println("3");
+    System.out.println("2");
+    System.out.println("1");
+    System.out.println("0");
+    System.out.println("haha car go boom");
+  }
+
+  public void drive() {
+    if (willExplode()) {
+      explode();
+    } else {
+      System.out.println("vroom");
+      mileage++;
+    }
+  }
+}
---
+++ 2021/04_oop/brandon_phiong/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    Car myCar = new Car("Tesla");
+    System.out.println(myCar);
+
+    for (int i = 0; i < 21; i++) {
+      myCar.drive();
+     }
+   }
+ }
+
