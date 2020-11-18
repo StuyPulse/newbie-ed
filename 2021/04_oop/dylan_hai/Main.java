704cd12a36df3819b6144a8e9757b32772678781
Dylan Hai <dylanhai@Dylans-Air.home>
2020-11-17 19:22:03 -0500

    Update

---
+++ 2021/04_oop/dylan_hai/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+      Car dylanCar = new Car("BMW 325i", "blue");
+      for(int i = 0; i<=20; i++){
+        dylanCar.drive();
+      }
+    }
+}
