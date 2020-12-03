ee8a8e9b27064d6f774c2d8c1aac1891fdf4d0e2
Eric Lin <ericlin@Erics-MBP.lan>
2020-12-03 08:57:06 -0500

    updated Hero.java, Mage.java, Beserker.java, and Mage.java to contain less errors (albeit still quite a few)

--- 2021/05_inheritance/Eric_Lin/Beserker.java
+++ 2021/05_inheritance/Eric_Lin/Beserker.java
@@ -1,6 +1,6 @@
 public class Beserker extends Hero{
     public Berserker(String name, int atk, int maxHP){
-        super(name, currentHP, maxHP, atk, "rage", currentResource, 50)
+        super(name, currentHP, maxHP, atk, "rage", currentResource, 50);
     }
     @override
     public int getAtk(){
@@ -13,7 +13,7 @@ public class Beserker extends Hero{
     public burst(int n){
         setCurrentResource(0);
         int n = getCurrentResource();
-        return useAbility("burst", 0)
+        return useAbility("burst", 0);
     }
     public heal(){
         setCurrentHP(getCurrentResource() + getCurrentHP());
--- 2021/05_inheritance/Eric_Lin/Mage.java
+++ 2021/05_inheritance/Eric_Lin/Mage.java
@@ -1,12 +1,12 @@
 public class Mage extends Hero{
     public Mage(String name, int atk, int maxHP){
-        super(name, currentHP, maxHP, atk, "mana", currentResource, 100)
+        super(name, currentHP, maxHP, atk, "mana", currentResource, 100);
     }
     public fireball(){
         return useAbility("fireball", 70);
         setCurrentResource(getCurrentResource()-75);
     }
     public recoverMana(){
-        setCurrentResource(getMaxResource())
+        setCurrentResource(getMaxResource());
     }
 }
