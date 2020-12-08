7e8fa3dec99ef15faac59d9af1e18480d997488a
Brandon Phiongl <onename548@gmail.com>
2020-12-08 18:26:33 -0500

    Made boxbugbrandon

---
+++ 2021/06_git2/group3/Brandon/BoxBugBrandon.java
@@ -0,0 +1,41 @@
+package group3.Brandon;
+
+import info.gridworld.actor.Bug;
+
+public class BoxBugBrandon extends Bug {
+    int steps; // a counter for how many steps we have taken
+    int length; // how far the bug will move
+
+    public BoxBugBrandon(int length) {
+        this.steps = 0;
+        this.length = length;
+    }
+
+    public BoxBugBrandon() {
+        this.steps = 0;
+        this.length = 4; // 4 is the default
+    }
+
+    // this is a helper funtion
+    // it was not in the original Bug class
+    private void quarterTurn() {
+        for(int i = 0; i < 2; i++) {
+            turn();
+        }
+    }
+
+    // this method will run once every loop
+    @Override
+    public void act() {
+        if(steps < length && canMove()) {
+            // move and increment steps
+            move();
+            steps++;
+        }
+        else {
+            // turn around and set counter to 0
+            quarterTurn();
+            steps = 0;
+        }
+    }
+}
