22f5b367078a8831f2b2f5d59ff346ba0567cb4d
Timothy So <so.timothy51@gmail.com>
2020-12-08 19:16:00 -0500

    Some minor changes, added comments everywhere.

--- 2021/06_git2/group2/Timothy/BoxBug.java
+++ 2021/06_git2/group2/Timothy/BoxBug.java
@@ -3,39 +3,39 @@ package group2.Timothy;
 import info.gridworld.actor.Bug;
 
 public class BoxBug extends Bug {
-    int steps; // a counter for how many steps we have taken
-    int length; // how far the bug will move
+    int steps;  // counter variable, tracks steps taken
+    int length; // distance bug moves before changing direction
 
+    //// CONSTRUCTOR(S)
+
+    // given length
     public BoxBug(int length) {
         this.steps = 0;
         this.length = length;
     }
 
+    // default constructor
     public BoxBug() {
         this.steps = 0;
-        this.length = 4; // 4 is the default
+        this.length = 4;    // default, can be changed
     }
 
-    // this is a helper funtion
-    // it was not in the original Bug class
-    private void quarterTurn() {
-        for(int i = 0; i < 2; i++) {
-            turn();
+    // helper function, dictates movement
+    private void rightTurn() {
+        for(int i = 0; i < 2; i++) {    // loop twice, 45x2=90
+            turn();                     // rotate 45 degrees clockwise
         }
     }
 
-    // this method will run once every loop
+    // runs once per loop, dictates movement
     @Override
     public void act() {
-        if(steps < length && canMove()) {
-            // move and increment steps
-            move();
-            steps++;
-        }
-        else {
-            // turn around and set counter to 0
-            quarterTurn();
-            steps = 0;
+        if(steps < length && canMove()) {   // conditions must be met before continuing
+            move();         // move in target direction
+            steps++;        // increment steps counter
+        } else {
+            rightTurn();    // make a right
+            steps = 0;      // reset steps
         }
     }
 }
--- 2021/06_git2/group2/Timothy/BoxBugRunner.java
+++ 2021/06_git2/group2/Timothy/BoxBugRunner.java
@@ -7,13 +7,16 @@ import info.gridworld.grid.Location;
 
 public class BoxBugRunner {
     public static void main(String[] args) {
+        // create visual workspace
         ActorWorld world = new ActorWorld();
+
         // add our two bugs
-        world.add(new Location(5, 2), new BoxBug());
-        BoxBug foo = new BoxBug(2);
-        // changing color
-        foo.setColor(Color.YELLOW);
-        world.add(new Location(5, 6), foo);
-        world.show();
+        // bug #1
+        world.add(new Location(5, 2), new BoxBug());    // preset spawn point, default constructor
+        // bug #2
+        BoxBug foo = new BoxBug(2);                     // unset spawn point, given length
+        foo.setColor(Color.YELLOW);                     // change color
+        world.add(new Location(5, 6), foo);             // set spawn point
+        world.show();                                   // display
     }
 }
