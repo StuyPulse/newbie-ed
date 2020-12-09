52701733848fd54a89cdf6c69ba26ed7e45e817e
Timothy So <so.timothy51@gmail.com>
2020-12-08 19:04:40 -0500

    Copy over newbie work, prepare for changes.

---
+++ 2021/06_git2/group2/Timothy/BoxBug.java
@@ -0,0 +1,41 @@
+package group2.Timothy;
+
+import info.gridworld.actor.Bug;
+
+public class BoxBug extends Bug {
+    int steps; // a counter for how many steps we have taken
+    int length; // how far the bug will move
+
+    public BoxBug(int length) {
+        this.steps = 0;
+        this.length = length;
+    }
+
+    public BoxBug() {
+        this.steps = 0;
+        this.length = 4; // 4 is the default
+    }
+
+    // this is a helper funtion
+    // it was not in the original Bug class
+    private void quarterTurn() {
+        for(int i = 0; i < 2; i++) {
+            turn();
+        }
+    }
+
+    // this method will run once every loop
+    @Override
+    public void act() {
+        if(steps < length && canMove()) {
+            // move and increment steps
+            move();
+            steps++;
+        }
+        else {
+            // turn around and set counter to 0
+            quarterTurn();
+            steps = 0;
+        }
+    }
+}
