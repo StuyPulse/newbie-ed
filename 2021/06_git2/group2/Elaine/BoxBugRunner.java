6eb8fc606adbeaa93d8985ec1753671f627965de
Timothy So <so.timothy51@gmail.com>
2020-12-08 19:03:38 -0500

    Correct package statement (oops).

--- 2021/06_git2/group2/Elaine/BoxBugRunner.java
+++ 2021/06_git2/group2/Elaine/BoxBugRunner.java
@@ -1,4 +1,4 @@
-package group2;
+package group2.Elaine;
 
 import java.awt.Color;
 
@@ -7,13 +7,13 @@ import info.gridworld.grid.Location;
 
 public class BoxBugRunner {
     public static void main(String[] args) {
-        ActorWorld world = new ActorWorld(); 
-        // add our two bugs 
+        ActorWorld world = new ActorWorld();
+        // add our two bugs
         world.add(new Location(5, 2), new BoxBug());
-        BoxBug foo = new BoxBug(2); 
+        BoxBug foo = new BoxBug(2);
         // changing color
         foo.setColor(Color.YELLOW);
         world.add(new Location(5, 6), foo);
-        world.show(); 
+        world.show();
     }
 }
